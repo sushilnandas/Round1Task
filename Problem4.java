aa52f5616236aa7543f2cece7efc282f400c1815
Sushilnandas <88280294+sushilnandas@users.noreply.github.com>
2025-08-27 13:30:45 +0530

    Problem4.java

--- Problem4.java
+++ Problem4.java
@@ -4,8 +4,6 @@ import java.util.*;
 public class Problem4 {
     public static void main(String[] args) {
         int[] arr = {1,2,8,9,12,46,76,82,15,20,30};
-
-        // dictionary-like result (using array index 1..9)
         int[] count = new int[10]; 
 
         for (int k = 1; k <= 9; k++) {
@@ -18,9 +16,9 @@ public class Problem4 {
             count[k] = c;
         }
 
-        // print result
         for (int k = 1; k <= 9; k++) {
             System.out.println(k + ": " + count[k]);
         }
     }
 }
+
